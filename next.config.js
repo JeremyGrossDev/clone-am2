cd214a36260dd247a5834882d2aa2c4fafeeb44f
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2021-05-25 19:57:51 -0600

    Day 1

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "pngimg.com",
+      "images-eu.ssl-images-amazon.com",
+      "whitebox.com",
+      "fakestoreapi.com",
+    ],
+  },
+};
+
+//https://links.papareact.com/f90
