40b9eeaa302ef94080e1910ae0cf7f6e91c159ff
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2021-05-31 13:12:47 -0600

    Day 3 fix Public key - take 1

--- src/components/Order.js
+++ src/components/Order.js
@@ -29,13 +29,19 @@ function Order({ id, amount, amountShipping, items, timestamp, images }) {
 
       <div className="p-5 sm:p-10">
         <div className="flex space-x-6 overflow-x-auto">
-          {images.map((image) => (
-            <img src={image} alt="" className="h-20 object-contain sm:h-32" />
+          {images.map((image, i) => (
+            <img
+              key={i}
+              src={image}
+              alt=""
+              className="h-20 object-contain sm:h-32"
+            />
           ))}
         </div>
       </div>
     </div>
   );
+  s;
 }
 
 export default Order;
